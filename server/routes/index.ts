9982a9ab6951d330db2009fd573f2a57ea366304
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2021-09-20 14:51:45 +0100

    Use S3Select to read visualisation data
    For the second test visualisation data is read from a CSV file in S3
    using S3Select to filter the data.
    
    The filtered data is then injected into the Vega visualisation before
    being rendered as before.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,7 +2,7 @@ import type { RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import getS3Client from '../utils/s3'
-import { getViz1 } from '../utils/visualisations'
+import { getViz1, getViz2 } from '../utils/visualisations'
 
 export default function routes(router: Router): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
@@ -11,13 +11,14 @@ export default function routes(router: Router): Router {
     const s3Client = getS3Client()
 
     const viz1 = await getViz1(s3Client)
+    const viz2 = await getViz2(s3Client)
 
     // const svgFile = createWriteStream('/tmp/vega-output.svg')
     // svgFile.write(viz1)
 
     res.locals = {
       vis1: viz1,
-      vis2: viz1,
+      vis2: viz2,
     }
     res.render('pages/index')
   })
--- server/utils/visualisations.ts
+++ server/utils/visualisations.ts
@@ -1,11 +1,12 @@
 import { Readable } from 'stream'
 
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { GetObjectCommand, S3, SelectObjectContentCommandInput } from '@aws-sdk/client-s3'
 import * as vega from 'vega'
 
+import { StringDecoder } from 'string_decoder'
 import config from '../config'
 
-export async function getViz1(s3Client: S3Client): Promise<string> {
+export async function getViz1(s3Client: S3): Promise<string> {
   // Example Vega bar chart visualisation:
   // https://github.com/vega/vega/blob/master/docs/examples/bar-chart.vg.json
   const data = await getData(s3Client, 'sandbox/bar-chart.vg.json')
@@ -15,11 +16,63 @@ export async function getViz1(s3Client: S3Client): Promise<string> {
   return view.toSVG()
 }
 
-export function getViz2(): vega.Spec {
-  return {}
+export async function getViz2(s3Client: S3): Promise<string> {
+  // Example Vega bar chart visualisation:
+  // https://github.com/vega/vega/blob/master/docs/examples/bar-chart.vg.json
+  const specData = await getData(s3Client, 'sandbox/bar-chart.vg.json')
+  const spec = JSON.parse(specData)
+
+  // TODO: Updated fill colour to distinguish 2nd visualisation
+  spec.marks[0].encode.update.fill.value = 'purple'
+
+  // DEBUG
+  const allData = await getData(s3Client, 'sandbox/viz2-data.csv')
+  console.dir('all data in CSV')
+  console.dir(allData)
+
+  const s3selectParams: SelectObjectContentCommandInput = {
+    Bucket: config.s3.bucket_name,
+    Key: 'sandbox/viz2-data.csv',
+    ExpressionType: 'SQL',
+    Expression: `SELECT category, amount FROM s3object`,
+    // Expression: `SELECT category, amount FROM s3object WHERE category IN ('A', 'B', 'I', 'L')`,
+    // Expression: `SELECT category, amount FROM s3object WHERE category IN ('Z')`,
+    // Expression: `SELECT category, amount FROM s3object WHERE CAST(amount AS NUMERIC) < 50`,
+    // Expression: `SELECT category, amount FROM s3object WHERE amount < 50`, // This doesn't work, need CAST
+    InputSerialization: {
+      CSV: {
+        FileHeaderInfo: 'USE',
+        RecordDelimiter: '\n',
+        FieldDelimiter: ',',
+      },
+    },
+    OutputSerialization: {
+      JSON: {
+        RecordDelimiter: ',',
+      },
+    },
+  }
+
+  const s3SelectResult = await s3Client.selectObjectContent(s3selectParams)
+
+  const readable = Readable.from(s3SelectResult.Payload)
+  const data = await getS3selectData(readable)
+
+  console.dir('filtered data')
+  console.dir(data)
+
+  spec.data = [
+    {
+      name: 'table',
+      values: data,
+    },
+  ]
+
+  const view = new vega.View(vega.parse(spec))
+  return view.toSVG()
 }
 
-async function getData(s3Client: S3Client, s3key: string): Promise<string> {
+async function getData(s3Client: S3, s3key: string): Promise<string> {
   const getObjectCommand = new GetObjectCommand({
     Bucket: config.s3.bucket_name,
     Key: s3key,
@@ -27,6 +80,34 @@ async function getData(s3Client: S3Client, s3key: string): Promise<string> {
 
   const getObjectResult = await s3Client.send(getObjectCommand)
   return readStream(getObjectResult.Body as Readable)
+}
+
+function getS3selectData<T>(readable: Readable): Promise<Array<T>> {
+  let jsonString = '['
+  const decoder = new StringDecoder('utf8')
+
+  return new Promise<Array<T>>(resolve => {
+    readable.on('data', chunk => {
+      if (chunk.Records) {
+        // Decode Uint8Array into a UTF-8 string
+        jsonString += decoder.write(Buffer.from(chunk.Records.Payload))
+      }
+    })
+
+    readable.on('end', () => {
+      jsonString += decoder.end()
+
+      if (jsonString.length > 1) {
+        // Remove trailing comma (not allowed in JSON)
+        jsonString = jsonString.slice(0, -1)
+        jsonString += ']'
+        resolve(JSON.parse(jsonString))
+      } else {
+        // No data
+        resolve([])
+      }
+    })
+  })
 }
 
 function readStream(readable: Readable): Promise<string> {
