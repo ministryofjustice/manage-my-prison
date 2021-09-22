e91bb226fd9a35e72a3f83485309d53e665e2241
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2021-09-22 11:44:36 +0100

    Added example of Vega-Lite visualisations
    Vega-Lite is an higher-level syntax to define visualisations.
    In this example the Vega-Lite spec is converted into a Vega spec which
    is then rendered.
    
    NOTE: In this example the Vega-Lite spec is hardcoded and the data is
    also embedded into the specification (e.g. they're not read from S3).
    The point of this example is to show how to convert/render a Vega-Lite
    visualisation, but of course this and its data can come from S3 as in
    the 2nd example.
    
    I've also added some documentation and types.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,7 +2,7 @@ import type { RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import getS3Client from '../utils/s3'
-import { getViz1, getViz2 } from '../utils/visualisations'
+import { getViz1, getViz2, getViz3 } from '../utils/visualisations'
 
 export default function routes(router: Router): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
@@ -12,6 +12,7 @@ export default function routes(router: Router): Router {
 
     const viz1 = await getViz1(s3Client)
     const viz2 = await getViz2(s3Client)
+    const viz3 = await getViz3()
 
     // const svgFile = createWriteStream('/tmp/vega-output.svg')
     // svgFile.write(viz1)
@@ -19,6 +20,7 @@ export default function routes(router: Router): Router {
     res.locals = {
       vis1: viz1,
       vis2: viz2,
+      vis3: viz3,
     }
     res.render('pages/index')
   })
--- server/utils/visualisations.ts
+++ server/utils/visualisations.ts
@@ -2,20 +2,40 @@ import { Readable } from 'stream'
 
 import { GetObjectCommand, S3, SelectObjectContentCommandInput } from '@aws-sdk/client-s3'
 import * as vega from 'vega'
+import * as vegaLite from 'vega-lite'
 
 import { StringDecoder } from 'string_decoder'
 import config from '../config'
 
+/**
+ * Gets an example visualisation from S3 and returns the rendered SVG.
+ *
+ * Data is embedded/hardcoded into the visualisation in this example.
+ *
+ * @param s3Client S3 client to use to get the visualisation.
+ * @returns a Promise<string> with the SVG of the rendered visualisation.
+ */
 export async function getViz1(s3Client: S3): Promise<string> {
   // Example Vega bar chart visualisation:
   // https://github.com/vega/vega/blob/master/docs/examples/bar-chart.vg.json
   const data = await getData(s3Client, 'sandbox/bar-chart.vg.json')
-  const spec = JSON.parse(data)
+  const spec: vega.Spec = JSON.parse(data)
 
   const view = new vega.View(vega.parse(spec))
   return view.toSVG()
 }
 
+/**
+ * Gets an example visualisation from S3, gets data from S3 using S3 Select and
+ * returns the rendered visualisation.
+ *
+ * Some example of S3 Select queries are also included (commented) in the code.
+ * An example of a possible way to filter data in Vega is also included
+ * (commented) in the code.
+ *
+ * @param s3Client S3 client to use to get the visualisation.
+ * @returns a Promise<string> with the SVG of the rendered visualisation.
+ */
 export async function getViz2(s3Client: S3): Promise<string> {
   // Example Vega bar chart visualisation:
   // https://github.com/vega/vega/blob/master/docs/examples/bar-chart.vg.json
@@ -77,6 +97,49 @@ export async function getViz2(s3Client: S3): Promise<string> {
   ]
 
   const view = new vega.View(vega.parse(spec))
+  return view.toSVG()
+}
+
+/**
+ * This example converts a VegaLite spec into Vega spec before rendering it.
+ *
+ * VegaLite is an higher level syntax for definiting visualisations (compared to than Vega).
+ *
+ * Spec and data are hardcoded in this example, it's mainly for the purpose of
+ * showing how to convert VegaLite specs into Vega specs for rendering.
+ *
+ * @returns a Promise<string> with the SVG of the rendered visualisation.
+ */
+export function getViz3(): Promise<string> {
+  const vegaLiteSpec: vegaLite.TopLevelSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'A simple bar chart with embedded data.',
+    width: 400,
+    height: 200,
+    data: {
+      values: [
+        { a: 'A', b: 28 },
+        { a: 'B', b: 55 },
+        { a: 'C', b: 43 },
+        { a: 'D', b: 91 },
+        { a: 'E', b: 81 },
+        { a: 'F', b: 53 },
+        { a: 'G', b: 19 },
+        { a: 'H', b: 87 },
+        { a: 'I', b: 52 },
+      ],
+    },
+    mark: 'bar',
+    encoding: {
+      x: { field: 'a', type: 'ordinal' },
+      y: { field: 'b', type: 'quantitative' },
+    },
+  }
+
+  // `vega-lite`'s `compile()` converts a Vega-Lite specification into a Vega one
+  const { spec: vegaSpec } = vegaLite.compile(vegaLiteSpec)
+
+  const view = new vega.View(vega.parse(vegaSpec))
   return view.toSVG()
 }
 
