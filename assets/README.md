f4af6a12345d72aa5bb56f6b7c0c1c34977cef02
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-09-28 17:50:40 +0100

    Normalise static asset routes Previously: • sass source files were reachable, whilst only built stylesheets should be • multiple routes existed to the same stylesheet and javascript files, whereas there should be one systematic pattern
    Now:
    • only particular sub-paths are mapped (`images`, `stylesheets` and `js`) excluding source files (`sass` and others in future)
    • each asset file can be reached via only one route reducing

---
+++ assets/README.md
@@ -0,0 +1,12 @@
+Browser assets
+==============
+
+These paths are mapped directly to url prefixes and _all_ files within are served:
+
+- `/assets/images`
+- `/assets/js`
+- `/assets/stylesheets`
+
+These paths are for source files and contents are not served directly:
+
+- `/assets/sass`
--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -12,10 +12,10 @@ export default function setUpStaticResources(): Router {
 
   //  Static Resources Configuration
   const cacheControl = { maxAge: config.staticResourceCacheDuration * 1000 }
+  ;['/assets/images', '/assets/js', '/assets/stylesheets'].forEach(dir => {
+    router.use(dir, express.static(path.join(process.cwd(), dir), cacheControl))
+  })
   ;[
-    '/assets',
-    '/assets/stylesheets',
-    '/assets/js',
     '/node_modules/govuk-frontend/govuk/assets',
     '/node_modules/govuk-frontend',
     '/node_modules/@ministryofjustice/frontend/moj/assets',
@@ -23,8 +23,12 @@ export default function setUpStaticResources(): Router {
   ].forEach(dir => {
     router.use('/assets', express.static(path.join(process.cwd(), dir), cacheControl))
   })
-  ;['/node_modules/jquery/dist/jquery.min.js'].forEach(dir => {
-    router.use('/assets/js/jquery.min.js', express.static(path.join(process.cwd(), dir), cacheControl))
+  ;[
+    ['/assets/images/favicon.ico', '/favicon.ico'],
+    ['/node_modules/jquery/dist/jquery.min.js', '/assets/js/jquery.min.js'],
+  ].forEach(mapping => {
+    const [filePath, urlPath] = mapping
+    router.use(urlPath, express.static(path.join(process.cwd(), filePath), cacheControl))
   })
 
   // Don't cache dynamic resources
