3c696271d7ce57f144f577bb20fe364f16890ce2
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-10-11 15:22:02 +0100

    Use async iterators to read data from S3 consistently between the two existing client methods

--- server/data/s3Client.ts
+++ server/data/s3Client.ts
@@ -6,7 +6,6 @@ import {
   SelectObjectContentCommand,
   SelectObjectContentCommandInput,
 } from '@aws-sdk/client-s3'
-import getStream from 'get-stream'
 
 export interface S3BucketConfig {
   accessKeyId: string
@@ -39,15 +38,20 @@ export default class S3Client {
       Key: key,
     })
     const response = await this.s3.send(command)
-    return (await getStream.buffer(response.Body as Readable)).toString()
+    const readableBody = response.Body as Readable
+    const chunks: Uint8Array[] = []
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const chunk of readableBody) {
+      chunks.push(chunk)
+    }
+    return Buffer.concat(chunks).toString('utf8')
   }
 
   async selectObjectContent(s3selectParams: SelectObjectContentCommandInput): Promise<unknown[]> {
     const command = new SelectObjectContentCommand(s3selectParams)
     const response = await this.s3.send(command)
-
     const records: Uint8Array[] = []
-    /* eslint-disable-next-line */
+    // eslint-disable-next-line no-restricted-syntax
     for await (const event of response.Payload) {
       if (event.Records) {
         records.push(event.Records.Payload)
