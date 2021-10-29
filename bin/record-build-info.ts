a72e9a9dbe6eda17cf1fd495cc4ccf7ef8168d7d
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-10-29 13:12:43 +0100

    Convert JS bin script to TS to benefit from type-checking

--- bin/record-build-info.ts
+++ bin/record-build-info.ts
@@ -1,18 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/env npx ts-node
+import { writeFileSync } from 'fs'
+import { resolve } from 'path'
 
-const fs = require('fs')
-const path = require('path')
+const target = resolve(process.cwd(), 'build-info.json')
 
-const target = path.resolve(__dirname, '../build-info.json')
-
-function getEnvironmentVariable(name) {
+function getEnvironmentVariable(name: string): string {
   if (process.env[name]) {
     return process.env[name]
   }
   throw new Error(`Missing env var ${name}`)
 }
 
-fs.writeFileSync(
+writeFileSync(
   target,
   JSON.stringify({
     buildNumber: getEnvironmentVariable('BUILD_NUMBER'),
