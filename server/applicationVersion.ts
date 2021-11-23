18efc35455d53832e364bafcc475d0d64931f280
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-11-23 14:24:12 +0000

    Read package meta info directly as strings …without an extra decoding step

--- server/applicationVersion.ts
+++ server/applicationVersion.ts
@@ -1,9 +1,9 @@
 // eslint-disable import/no-unresolved,global-require
 import fs from 'fs'
 
-const packageData = JSON.parse(fs.readFileSync('./package.json').toString())
+const packageData = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }))
 const { buildNumber, gitRef } = fs.existsSync('./build-info.json')
-  ? JSON.parse(fs.readFileSync('./build-info.json').toString())
+  ? JSON.parse(fs.readFileSync('./build-info.json', { encoding: 'utf8' }))
   : {
       buildNumber: packageData.version,
       gitRef: 'unknown',
